de78968f4c3e3c1279e7d8e0777827b89ef8c790
erzhena-d <dyrkheevaerzhena@gmail.com>
2025-07-11 02:57:41 +0000

    changes

--- Homework-2/apache.sh
+++ Homework-2/apache.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+!/bin/bash
 
-# sudo apt update
-# sudo apt install -y apache2
-# sudo systemctl restart apache2
-# sudo systemctl enable apache2
-# echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+sudo apt update
+sudo apt install -y apache2
+sudo systemctl restart apache2
+sudo systemctl enable apache2
+echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
--- Homework-2/vpc.tf
+++ Homework-2/vpc.tf
@@ -1,16 +1,35 @@
 
-# data "aws_subnets" "all" {
-#   filter {
-#     name   = "vpc-id"
-#     values = [var.vpc_id]
-#   }
+data "aws_vpc" "default" {
+  default = true
+}
 
-  
+data "aws_subnets" "available" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+locals {
+  subnets = slice(data.aws_subnets.available.ids, 0, 3)
+}
+
+
+resource "aws_instance" "web" {
+  count         = 3
+  ami           = "ami-0ec1bf4a8f92e7bd1"
+  instance_type = "t3.micro"
+  subnet_id     = local.subnets[count.index]
+  key_name      = aws_key_pair.bastion-key.key_name
+  security_groups = [aws_security_group.allow_tls.id]
+  user_data = file("apache.sh")
+
+  tags = {
+    Name = "web-${count.index + 1}"
+  }
+}
+
+# locals {
+#   subnets = slice(data.aws_subnets.available.ids, 0, 3)
 # }
 
-# resource "aws_instance" "app" {
-#   for_each      = toset(data.aws_subnets.all.ids)
-#   ami           = var.ami
-#   instance_type = "t2.micro"
-#   subnet_id     = each.value
-# }
