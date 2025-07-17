7b19466129b876deba35fb6f42c7ac0da8ec2c9f
erzhena-d <dyrkheevaerzhena@gmail.com>
2025-07-17 16:53:13 +0000

    homework 22

--- Homework-2/provider.tf
+++ Homework-2/provider.tf
@@ -14,5 +14,5 @@ provider "aws" {
 
 resource "aws_key_pair" "bastion-key" {
   key_name   = "bastion-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCZ9hs7Q5PvvpTNMNnq4aghQMlf2OmNKHv+foFz+9DAb3w51dY8atoKfvf6zxjiWnoULJ+DyT+ZWncGid/+d62CLyLsXK7iZXGLQo33bu3aNW3D9K2ewUbwuxzYDkzGB90JcsKuIJXHcLj+seayVTvxsZyiltKaP/zZUSu2d0XaWtCaE0ZCjth/B1gOKQRFyWy850RnURXPlsIh3QAwRz4qSc0VE4S8Uc9W+faWk12Lb7eeKZPANc/Rzw11L8bZyexVGoOG863qUzJnBXsEJND4N6hRS20VyTv0BKQKtNatIlLHXh1GSOupx2j29Uxdp2uRcPGeqIzj0teT1toVthXEF2+sGywLrvxyYkSgcdTtQEVgCvjsjr9pNPpeQuKZEjWQtT+VAX9P4Qnk+l533LDgh3fnUX3QfLtT/HnUQOt3gHtYpJxxP79e73tiWZub6b1DsClhCPjBgyXxC8n5Q4Jbc77/Ul0n/b3sDLV/E7076vvB2Ypnm+AhU0rekpuuRgM= ec2-user@ip-172-31-23-112.ca-central-1.compute.internal"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
--- Homework-2/vpc.tf
+++ Homework-2/vpc.tf
@@ -3,33 +3,71 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "available" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-locals {
-  subnets = slice(data.aws_subnets.available.ids, 0, 3)
-}
-
-
-resource "aws_instance" "web" {
-  count         = 3
+resource "aws_instance" "web-1" {
   ami           = "ami-0ec1bf4a8f92e7bd1"
-  instance_type = "t3.micro"
-  subnet_id     = local.subnets[count.index]
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-00b3b0fba4adece83"
   key_name      = aws_key_pair.bastion-key.key_name
   security_groups = [aws_security_group.allow_tls.id]
   user_data = file("apache.sh")
 
   tags = {
-    Name = "web-${count.index + 1}"
+    Name = "web-1"
   }
 }
 
-# locals {
+resource "aws_instance" "web-2" {
+  ami           = "ami-0ec1bf4a8f92e7bd1"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-0f5fb1a78b738bfe0"
+  key_name      = aws_key_pair.bastion-key.key_name
+  security_groups = [aws_security_group.allow_tls.id]
+  user_data = file("apache.sh")
+
+  tags = {
+    Name = "web-2"
+  }
+}
+
+resource "aws_instance" "web-3" {
+  ami           = "ami-0ec1bf4a8f92e7bd1"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-031784cba0b9443bf"
+  key_name      = aws_key_pair.bastion-key.key_name
+  security_groups = [aws_security_group.allow_tls.id]
+  user_data = file("apache.sh")
+
+  tags = {
+    Name = "web-3"
+  }
+}
+
+output "ec2_global_ips" {
+  value = [
+    aws_instance.web-1.public_ip,
+    aws_instance.web-2.public_ip,
+    aws_instance.web-3.public_ip
+  ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+# tags = {
+#     Name = "web-${count.index + 1}"
+#   }
+
+
+# # locals {
 #   subnets = slice(data.aws_subnets.available.ids, 0, 3)
 # }
 
